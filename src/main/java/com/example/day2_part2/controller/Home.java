34b64849872da7295b67e47e0e7b4f17f97b42f7
ajnr2456 <arjunnagar486@gmail.com>
2026-01-19 15:31:10 +0530

    Edit our second API

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -4,11 +4,21 @@ import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"AJ","ajnr@gmail.com");
-        return student;
+    public List<StudentModel> getStudent1(){
+        List<StudentModel> l=new ArrayList<>();
+        l.add(new StudentModel(1,"AJ","ajnr@gmail.com"));
+        l.add(new StudentModel(2,"ARJUN","arjun@gmail.com"));
+        l.add(new StudentModel(3,"AARAV","aarav@gmail.com"));
+        l.add(new StudentModel(4,"ABHI","abhi@gmail.com"));
+        l.add(new StudentModel(5,"AJAY","ajay@gmail.com"));
+        return l;
+
     }
 }
+
